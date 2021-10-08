c540148e6f5151a434749b5530f86e0a97cb9e10
Your Name <you@example.com>
2021-10-08 15:15:50 +0700

    update card blog, add view into card blog

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -7,19 +7,23 @@ function BlogCard() {
             <div class="card flex-fill">
                 <img alt="Card Image" src={sampleImage} class="card-img-top" />
                 <div class="card-header">
-                    <h5 class="card-title mb-0">Card with image and button</h5>
+                    <h5 class="card-title mb-0">
+                        Card with image and button
+                        <i class="far fa-eye float-right"> 6969</i>
+                    </h5>
                 </div>
                 <div class="card-body">
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <ul class="available-info">
-                        <li>
-                            <i class="fas fa-user"></i> Ruby Perrin
-                        </li>
-                        <li>
-                            <i class="far fa-clock"></i> 1 minute ago
-                        </li>
-                    </ul>
-                    <a class="btn btn-primary" href="#">View</a>
+                    <h5 class="table-avatar">
+                        <a href="#" class="avatar avatar-xs mr-2 float-left">
+                            <img class="avatar-img rounded-circle" src="assets/img/img-01.jpg" alt="User Image" />
+                        </a>
+                        <a href="#" class="float-left">
+                            <span> Mr. Ruby Perrin </span><br />
+                            <i class="far fa-clock"> 1 minute ago</i><br />
+                        </a>
+                    </h5>
+                    <a class="btn btn-primary float-right" href="#">View</a>
                 </div>
             </div>
         </div>
