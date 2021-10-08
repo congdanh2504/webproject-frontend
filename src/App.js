2011f79321368e6fc74bd625ec82ef5ff8bf97fa
congdanh2504 <danhuchiha2002@gmail.com>
2021-10-08 15:32:48 -0700

    add not found page

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Footer from "./components/Footer";
 import EmployerRegister from "./components/Employer-Register";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Router>
-      <Nav />
+      <Nav/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/employer-register" component={EmployerRegister} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/blog" component={Blog} />
+        <Route component={NotFound} />
       </Switch>
       <Footer />
     </Router>
