e72afa05eefbb79f16ecfa0ba3c525b55424dc0d
vthcvn <61310039+vthcvn@users.noreply.github.com>
2021-10-09 09:49:09 +0700

    update home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -513,6 +513,79 @@ function Home() {
         </div>
       </section>
       {/* EndPopular Section */}
+
+      {/* <!-- Availabe Features --> */}
+      <section class="section section-features">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-5 features-img">
+              <img
+                src="assets/img/features/feature.png"
+                class="img-fluid"
+                alt="Feature"
+              />
+            </div>
+            <div class="col-md-7">
+              <div class="section-header">
+                <h2 class="mt-2">Availabe Features in Our Clinic</h2>
+                <p>
+                  It is a long established fact that a reader will be distracted
+                  by the readable content of a page when looking at its layout.{" "}
+                </p>
+              </div>
+              {/* <div class="features-slider slider"> */}
+                <Slider {...settings_clinic_specialities}>
+                  <div class="feature-item text-center">
+                    <img
+                      src="assets/img/features/feature-01.jpg"
+                      class="img-fluid"
+                      alt="Feature"
+                    />
+                    <p>Patient Ward</p>
+                  </div>
+
+                  <div class="feature-item text-center">
+                    <img
+                      src="assets/img/features/feature-02.jpg"
+                      class="img-fluid"
+                      alt="Feature"
+                    />
+                    <p>Test Room</p>
+                  </div>
+
+                  <div class="feature-item text-center">
+                    <img
+                      src="assets/img/features/feature-03.jpg"
+                      class="img-fluid"
+                      alt="Feature"
+                    />
+                    <p>ICU</p>
+                  </div>
+
+                  <div class="feature-item text-center">
+                    <img
+                      src="assets/img/features/feature-04.jpg"
+                      class="img-fluid"
+                      alt="Feature"
+                    />
+                    <p>Laboratory</p>
+                  </div>
+
+                  <div class="feature-item text-center">
+                    <img
+                      src="assets/img/features/feature-05.jpg"
+                      class="img-fluid"
+                      alt="Feature"
+                    />
+                    <p>Operation</p>
+                  </div>
+                </Slider>
+              </div>
+            </div>
+          </div>
+        {/* </div> */}
+      </section>
+      {/* <!-- Availabe Features --> */}
     </>
   );
 }
