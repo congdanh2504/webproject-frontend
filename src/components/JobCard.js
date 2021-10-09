a8969f458bc900fa597a7a6e8481ad568cac1103
vthcvn <61310039+vthcvn@users.noreply.github.com>
2021-10-09 14:50:37 +0700

    update jobcard.js
    change the sentences, images, links

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -4,7 +4,7 @@ function JobCard() {
   return (
     <div class="profile-widget">
       <div class="doc-img">
-        <a href="doctor-profile.html">
+        <a href="">
           <img
             class="img-fluid"
             alt="User Image"
@@ -17,12 +17,10 @@ function JobCard() {
       </div>
       <div class="pro-content">
         <h3 class="title">
-          <a href="doctor-profile.html">Ruby Perrin</a>
+          <a href="">VKU group</a>
           <i class="fas fa-check-circle verified"></i>
         </h3>
-        <p class="speciality">
-          MDS - Periodontology and Oral Implantology, BDS
-        </p>
+        <p class="speciality">We have the right job for you</p>
         <div class="rating">
           <i class="fas fa-star filled"></i>
           <i class="fas fa-star filled"></i>
@@ -33,10 +31,10 @@ function JobCard() {
         </div>
         <ul class="available-info">
           <li>
-            <i class="fas fa-map-marker-alt"></i> Florida, USA
+            <i class="fas fa-map-marker-alt"></i> Danang, VN
           </li>
           <li>
-            <i class="far fa-clock"></i> Available on Fri, 22 Mar
+            <i class="far fa-clock"></i> Deadline Otc, 10
           </li>
           <li>
             <i class="far fa-money-bill-alt"></i> $300 - $1000
@@ -48,14 +46,9 @@ function JobCard() {
           </li>
         </ul>
         <div class="row row-sm">
-          <div class="col-6">
-            <a href="doctor-profile.html" class="btn view-btn">
-              View Profile
-            </a>
-          </div>
-          <div class="col-6">
-            <a href="booking.html" class="btn book-btn">
-              Book Now
+          <div class="col-12">
+            <a href="" class="btn view-btn">
+              See details
             </a>
           </div>
         </div>
