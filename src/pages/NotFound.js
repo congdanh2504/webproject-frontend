3504c6b1dbcdba8cfd403c90b5a7098c7f83e2d4
vthcvn <61310039+vthcvn@users.noreply.github.com>
2021-10-09 15:32:26 +0700

    update NotFound.js
    replace a tag with link tag

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function NotFound() {
     return (
@@ -9,7 +10,7 @@ function NotFound() {
                     <h1>404</h1>
                     <h3 class="h2 mb-3"><i class="fa fa-warning"></i> Oops! Page not found!</h3>
                     <p class="h4 font-weight-normal">The page you requested was not found.</p>
-                    <a href="index.html" class="btn btn-primary">Back to Home</a>
+                    <Link to="/" class="btn btn-primary">Back to Home</Link>
                 </div>
             
             </div>
