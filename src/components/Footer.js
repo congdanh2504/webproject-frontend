e5a9736ecfd609ec4ef92a9743403746e4c52909
vthcvn <61310039+vthcvn@users.noreply.github.com>
2021-10-09 14:56:56 +0700

    Update Footer.js
    replace a tag with link tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,109 +1,158 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
-    return (
-        <div>
-            <footer className="footer">
-                <div className="footer-top">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-lg-3 col-md-6">
-                                <div className="footer-widget footer-about">
-                                    <div className="footer-about-content">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                                        <div className="social-icon">
-                                            <ul>
-                                                <li>
-                                                    <a href="#" target="_blank"><i className="fab fa-facebook-f"></i> </a>
-                                                </li>
-                                                <li>
-                                                    <a href="#" target="_blank"><i className="fab fa-twitter"></i> </a>
-                                                </li>
-                                                <li>
-                                                    <a href="#" target="_blank"><i className="fab fa-linkedin-in"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#" target="_blank"><i className="fab fa-instagram"></i></a>
-                                                </li>
-                                                <li>
-                                                    <a href="#" target="_blank"><i className="fab fa-dribbble"></i> </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div className="col-lg-3 col-md-6">
-
-                                <div className="footer-widget footer-menu">
-                                    <h2 className="footer-title">For someone</h2>
-                                    <ul>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                    </ul>
-                                </div>
-
-                            </div>
-
-                            <div className="col-lg-3 col-md-6">
-
-                                <div className="footer-widget footer-menu">
-                                    <h2 className="footer-title">For someone</h2>
-                                    <ul>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                        <li><a href="#"><i className="fas fa-angle-double-right"></i> Link</a></li>
-                                    </ul>
-                                </div>
-
-                            </div>
-
-                            <div className="col-lg-3 col-md-6">
-                                <div className="footer-widget footer-contact">
-                                    <h2 className="footer-title">Contact Us</h2>
-                                    <div className="footer-contact-info">
-                                        <div className="footer-address">
-                                            <span><i className="fas fa-map-marker-alt"></i></span>
-                                            <p> 470 Tran Dai Nghia,<br/> Ngu Hanh Son, Da Nang </p>
-                                        </div>
-                                        <p>
-                                            <i className="fas fa-phone-alt"></i> +1 123 456 7890
-                                        </p>
-                                        <p className="mb-0">
-                                            <i className="fas fa-envelope"></i> jobhub@example.com
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
+  return (
+    <div>
+      <footer className="footer">
+        <div className="footer-top">
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="footer-widget footer-about">
+                  <div className="footer-about-content">
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua.{" "}
+                    </p>
+                    <div className="social-icon">
+                      <ul>
+                        <li>
+                          <a href="#" target="_blank">
+                            <i className="fab fa-facebook-f"></i>{" "}
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" target="_blank">
+                            <i className="fab fa-twitter"></i>{" "}
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" target="_blank">
+                            <i className="fab fa-linkedin-in"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" target="_blank">
+                            <i className="fab fa-instagram"></i>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" target="_blank">
+                            <i className="fab fa-dribbble"></i>{" "}
+                          </a>
+                        </li>
+                      </ul>
                     </div>
+                  </div>
                 </div>
+              </div>
 
-                <div className="footer-bottom">
-                    <div className="container-fluid">
+              <div className="col-lg-3 col-md-6">
+                <div className="footer-widget footer-menu">
+                  <h2 className="footer-title">For job seeker</h2>
+                  <ul>
+                    <li>
+                      <Link to="">
+                        <i className="fas fa-angle-double-right"></i> Jobs
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="" href="">
+                        <i className="fas fa-angle-double-right"></i> Top
+                        Companies
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/blog">
+                        <i className="fas fa-angle-double-right"></i> Blog
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/about">
+                        <i className="fas fa-angle-double-right"></i> About
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-                        <div className="copyright">
-                            <div className="copyright-menu">
-                                <ul className="policy-menu">
-                                    <li><a href="term-condition.html">Terms and Conditions</a></li>
-                                    <li><a href="privacy-policy.html">Policy</a></li>
-                                </ul>
-                            </div>
-                        </div>
+              <div className="col-lg-3 col-md-6">
+                <div className="footer-widget footer-menu">
+                  <h2 className="footer-title">For employer</h2>
+                  <ul>
+                    <li>
+                      <Link to="">
+                        <i className="fas fa-angle-double-right"></i> Jobs
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="" href="">
+                        <i className="fas fa-angle-double-right"></i> Top
+                        Companies
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/blog">
+                        <i className="fas fa-angle-double-right"></i> Blog
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/about">
+                        <i className="fas fa-angle-double-right"></i> About
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
+              <div className="col-lg-3 col-md-6">
+                <div className="footer-widget footer-contact">
+                  <h2 className="footer-title">Contact Us</h2>
+                  <div className="footer-contact-info">
+                    <div className="footer-address">
+                      <span>
+                        <i className="fas fa-map-marker-alt"></i>
+                      </span>
+                      <p>
+                        {" "}
+                        470 Tran Dai Nghia,
+                        <br /> Ngu Hanh Son, Da Nang{" "}
+                      </p>
                     </div>
+                    <p>
+                      <i className="fas fa-phone-alt"></i> +1 123 456 7890
+                    </p>
+                    <p className="mb-0">
+                      <i className="fas fa-envelope"></i> jobhub@example.com
+                    </p>
+                  </div>
                 </div>
-                </footer>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+
+        <div className="footer-bottom">
+          <div className="container-fluid">
+            <div className="copyright">
+              <div className="copyright-menu">
+                <ul className="policy-menu">
+                  <li>
+                    <a href="term-condition.html">Terms and Conditions</a>
+                  </li>
+                  <li>
+                    <a href="privacy-policy.html">Policy</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
