8b6389f8016393002f7a9dd0517220b752bb2b58
vthcvn <61310039+vthcvn@users.noreply.github.com>
2021-10-10 15:38:00 +0700

    update JobDetails.js
    Add component Breadcrumd

--- src/pages/JobDetails.js
+++ src/pages/JobDetails.js
@@ -1,31 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Breadcrumb from "../components/Breadcrumb";
 
 function JobDetails() {
   return (
     <>
-      {/* <!-- Breadcrumb --> */}
-      <div class="breadcrumb-bar">
-        <div class="container-fluid">
-          <div class="row align-items-center">
-            <div class="col-md-12 col-12">
-              <nav aria-label="breadcrumb" class="page-breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li class="breadcrumb-item active" aria-current="page">
-                    Job Details
-                  </li>
-                </ol>
-              </nav>
-              <h2 class="breadcrumb-title">Job Details</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <!-- /Breadcrumb --> */}
-
+      <Breadcrumb title="Job Details"></Breadcrumb>
       <div class="content">
         <div class="container">
           <div class="card">
