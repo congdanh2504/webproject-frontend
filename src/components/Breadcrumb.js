1ed5c9a5d4982aef1c2e7391c8fbec71e448ae2d
vthcvn <61310039+vthcvn@users.noreply.github.com>
2021-10-10 15:32:42 +0700

    create Breadcrumb.js
    create breadcrumb component

---
+++ src/components/Breadcrumb.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Breadcrumb(props) {
+  return (
+    <div class="breadcrumb-bar">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-md-12 col-12">
+            <nav aria-label="breadcrumb" class="page-breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                  <Link to="/">Home</Link>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                  {props.title}
+                </li>
+              </ol>
+            </nav>
+            <h2 class="breadcrumb-title">{props.title}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Breadcrumb;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Breadcrumb from "../components/Breadcrumb";
 function About() {
   return (
     <>
-      {/* <!-- Breadcrumb --> */}
-      <div class="breadcrumb-bar">
-        <div class="container-fluid">
-          <div class="row align-items-center">
-            <div class="col-md-12 col-12">
-              <nav aria-label="breadcrumb" class="page-breadcrumb">
-                <ol class="breadcrumb">
-                  <li class="breadcrumb-item">
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li class="breadcrumb-item active" aria-current="page">
-                    About
-                  </li>
-                </ol>
-              </nav>
-              <h2 class="breadcrumb-title">About</h2>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <!-- /Breadcrumb --> */}
-
+      <Breadcrumb title="About"></Breadcrumb>
       <div class="content">
         <div class="container">
           <div class="row">
