226e18c0a0387fc240c5aaf09057ddc0898154dd
vthcvn <61310039+vthcvn@users.noreply.github.com>
2021-10-10 14:37:16 +0700

    update JobDetails.js
    update google map

--- src/pages/JobDetails.js
+++ src/pages/JobDetails.js
@@ -175,8 +175,9 @@ function JobDetails() {
                 <div role="tabpanel" id="doc_locations" class="tab-pane fade">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d122708.985564692!2d108.11870716273522!3d16.031423630108062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3142196670000001%3A0x592b1fdb40a1cb95!2zTmdoxKlhIHRyYW5nIEjDsmEgUGjDoXQ!5e0!3m2!1svi!2s!4v1633850474257!5m2!1svi!2s"
-                    width="600"
+                    width="100%"
                     height="450"
+                    style={{border: "none"}}
                     allowfullscreen=""
                     loading="lazy"
                   ></iframe>
