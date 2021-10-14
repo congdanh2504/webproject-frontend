d9d3e09cc02af94fd46b962494500f5cdba29170
congdanh2504 <danhuchiha2002@gmail.com>
2021-10-13 18:56:28 -0700

    change form

--- src/components/ForgotPasswordForm.js
+++ src/components/ForgotPasswordForm.js
@@ -7,17 +7,14 @@ function ForgotPasswordForm() {
                 <h3>Forgot Password?</h3>
                 <p class="small text-muted">Enter your email to get a password reset link</p>
             </div>
-            
-            <form action="https://dreamguys.co.in/demo/doccure/login.html">
-                <div class="form-group form-focus">
-                    <input type="email" class="form-control floating"/>
-                    <label class="focus-label">Email</label>
-                </div>
-                <div class="text-right">
-                    <a class="forgot-link" href="login.html">Remember your password?</a>
-                </div>
-                <button class="btn btn-primary btn-block btn-lg login-btn" type="submit">Reset Password</button>
-            </form>
+            <div className="form-group">
+                <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                />
+            </div>
+            <button class="btn btn-primary btn-block btn-lg login-btn" type="submit">Reset Password</button>
             
         </div>
     )
