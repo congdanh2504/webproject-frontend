58d5625f6727e819cfa431b275d7330d6e53ebdc
congdanh2504 <danhuchiha2002@gmail.com>
2021-10-13 19:45:51 -0700

    add location api

--- src/pages/AddJob.js
+++ src/pages/AddJob.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import InputTag from '../components/InputTag'
 import TextAreaTag from '../components/TextAreaTag'
+import Select from 'react-select'
+import {getDistricts, getProvinces, getWards} from '../api/api'
 
 const AddJob = () => {
+  const [provinceOptions, changeProvinceOptions] = useState([]);
+  const [districtOptions, changeDistrictOptions] = useState([]);
+  const [wardOptions, changeWardOptions] = useState([]);
+  const [province, changeProvince] = useState("");
+  const updateProvince = (param) => {
+      changeProvince(param.label)
+      async function fetchDistricts() {
+          let response = await getDistricts(param.value)
+          changeDistrictOptions(response)
+      }
+      fetchDistricts()  
+  }
+
+  const [district, changeDistrict] = useState("");
+  const updateDistrict = (param) => {
+      changeDistrict(param.label)
+      async function fetchWards() {
+          let response = await getWards(param.value)
+          changeWardOptions(response)
+      }
+      fetchWards()  
+  }    
+
+  const [ward, changeWard] = useState("");
+  const updateWard = (param) => changeWard(param.label)
+  const [street, changeStreet] = useState("");
+  const updateStreet = (param) => changeStreet(param.target.value)
+  var defaultLocation = `${street},${ward},${district},${province},viet_nam`;
+  useEffect(() => {
+    async function fetchProvinces() {
+        let response = await getProvinces()
+        changeProvinceOptions(response)
+    }
+    fetchProvinces()      
+  }, [])
+
   return (
     <div class="content">
       <div class="container-fluid">
@@ -12,9 +50,18 @@ const AddJob = () => {
               <div class="card-body">
                 <form>
                   <div class="row form-row">
-                    <InputTag type='file' title="Image" />
-                    <InputTag type='text' title="Company" placeholder="Your company" />
-
+                    <div class="col-12 col-md-6">
+                      <div class="form-group">
+                        <label>Choose images</label>
+                        <input
+                          type="file"
+                          class="form-control"
+                          accept="image/*"
+                          multiple
+                        />
+                      </div>
+                    </div>
+                    <InputTag type='text' title="Title" placeholder="Title" />
                     <div class="col-12 col-md-6">
                       <div class="form-group">
                         <label>Types of Career</label>
@@ -29,7 +76,7 @@ const AddJob = () => {
                       </div>
                     </div>
 
-                    <InputTag type='text' title="Carrer" placeholder="Input the name of career!" />
+                    <InputTag type='text' title="Carrier" placeholder="Input the name of career!" />
                     <InputTag type='date' title="Duration" />
                     <InputTag type='number' title="Salary (USD)" />
                   </div>
@@ -37,33 +84,38 @@ const AddJob = () => {
                     <div className="col-12 col-md-6">
                       <div class="col-12">
                         <div class="form-group">
-                          <label>Tỉnh/ Thành phố</label>
-                          <select class="form-control select">
-                            <option>Select</option>
-                          </select>
+                          <label>Province</label>
+                          <Select placeholder="Province" options={provinceOptions}  onChange={updateProvince} />
                         </div>
                       </div>
                       <div class="col-12 ">
                         <div class="form-group">
-                          <label>Quận/ Huyện</label>
-                          <select class="form-control select">
-                            <option>Select</option>
-                          </select>
+                          <label>District</label>
+                          <Select placeholder="District" options={districtOptions}  onChange={updateDistrict}/>
                         </div>
                       </div>
                       <div class="col-12">
                         <div class="form-group">
-                          <label>Xã/Thị trấn</label>
-                          <select class="form-control select">
-                            <option>Select</option>
-                          </select>
+                          <label>Ward</label>
+                          <Select placeholder="Ward" options={wardOptions}  onChange={updateWard}/>
+                        </div>
+                      </div>
+                      <div class="col-12">
+                        <div class="form-group">
+                          <label>Street</label>
+                          <input class="form-control select" onChange={updateStreet} placeholder="Street">
+                          </input>
                         </div>
                       </div>
                     </div>
                     <div className="col-12 col-md-6 mh-100">
-                      <div className="w-100 h-100 bg-primary">
-                        <label>Google Maps</label>
-                        <h3>Google Maps is shown here!</h3>
+                      <div className="w-100 h-100">
+                        <iframe
+                          width="490" 
+                          height="350"
+                          loading="lazy"
+                          src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBT-FcupKSzJG1IuC4ZtNyQ-Qg0rdoY47k&q=${defaultLocation}`}>
+                        </iframe>
                       </div>
                     </div>
                   </div>
